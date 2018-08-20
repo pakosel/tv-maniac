d1102111103be1a7cf9caea37bedebb595af93d9
pawel.koselski <pawel.koselski@aptitudesoftware.com>
2018-08-20 17:03:07 +0200

    Ex. 3 - new component deleted and regenerated

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,15 @@
-import {Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'tm-home',
-  templateUrl: 'home.component.html'
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
 })
+export class HomeComponent implements OnInit {
 
-export class HomeComponent {
+  constructor() { }
+
+  ngOnInit() {
+  }
+
 }
