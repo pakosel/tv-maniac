b9e5ff16ab4e8ecbcf5c4dec7d295b46a6cd1ecc
pawel.koselski <pawel.koselski@aptitudesoftware.com>
2018-08-21 15:32:39 +0200

    Ex. 27 - extract component display to separate component and pass value from main component using @Input attribute

--- src/app/tv/tv.module.ts
+++ src/app/tv/tv.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchComponent } from './search/search.component';
 import {HttpClientModule} from '@angular/common/http';
+import { PosterComponent } from './poster/poster.component';
 
 @NgModule({
   imports: [
     CommonModule,
     HttpClientModule
   ],
-  declarations: [SearchComponent]
+  declarations: [SearchComponent, PosterComponent]
 })
 export class TvModule { }
