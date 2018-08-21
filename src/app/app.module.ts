f3f86c3efe5063b292c661183440cb9dde8a3046
pawel.koselski <pawel.koselski@aptitudesoftware.com>
2018-08-21 11:45:45 +0200

    Ex. 12 - generate new module and component. Commands: 'ng generate module tv -m=app' 'ng generate component tv/search'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HomeComponent} from './home/home.component';
 import { PagesModule } from './pages/pages.module';
+import { TvModule } from './tv/tv.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { PagesModule } from './pages/pages.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PagesModule
+    PagesModule,
+    TvModule
   ],
   providers: [],
   bootstrap: [AppComponent]
