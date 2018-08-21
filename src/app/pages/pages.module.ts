3102f9dca49f136641d33c2f706f4ef5d1f3635b
pawel.koselski <pawel.koselski@aptitudesoftware.com>
2018-08-21 11:42:39 +0200

    Ex. 11 - generate new component named "contact" and add it automatically to pages module. Command: 'ng generate component pages/contact'

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [ContactComponent]
 })
 export class PagesModule { }
