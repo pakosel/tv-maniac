481800eb2127bed554bc2c073379e8839895ef86
pawel.koselski <pawel.koselski@aptitudesoftware.com>
2018-08-21 14:28:39 +0200

    Ex. 22 - query tv maze with typed response

--- src/app/tv/tvmaze.service.ts
+++ src/app/tv/tvmaze.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {ShowResponse} from './tv.models';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +15,6 @@ export class TvmazeService {
 
   searchShows(query: string) {
     const url = `${this.apiRoot}/search/shows?q=${query}`;
-    this.http.get(url).subscribe(resp => console.table(resp));
+    this.http.get<ShowResponse[]>(url).subscribe(resp => console.table(resp));
   }
 }
