920daa4b88567e21e3fbb42488a9434beeea9d81
pawel.koselski <pawel.koselski@aptitudesoftware.com>
2018-08-22 09:32:30 +0200

    Ex. 33 - Create Contact form with model and bind form fields with model fields using ngModel

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+  contact = {
+    email: '',
+    message: ''
+  }
   constructor() { }
 
   ngOnInit() {
