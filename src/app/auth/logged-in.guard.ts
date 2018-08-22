e1fb2eec8eb407c37bbfde42fe3874bb8505ca7d
pawel.koselski <pawel.koselski@aptitudesoftware.com>
2018-08-22 16:32:51 +0200

    Ex. 59 - fix incorrect login (no auth) problem - redirect to home page

--- src/app/auth/logged-in.guard.ts
+++ src/app/auth/logged-in.guard.ts
@@ -1,13 +1,22 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router} from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoggedInGuard implements CanActivate {
+
+  constructor(private router: Router) {
+  }
+
   canActivate(next: ActivatedRouteSnapshot): boolean {
     const isLoggedIn = prompt('Are you logged in?') === 'yes';
+
+    if(!isLoggedIn) {
+      this.router.navigateByUrl('');
+    }
+
     return isLoggedIn;
   }
 }
