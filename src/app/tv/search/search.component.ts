8de28011ad0ae394d67a183a6560d43163b777ac
pawel.koselski <pawel.koselski@aptitudesoftware.com>
2018-08-22 11:22:26 +0200

    Ex. 39 - Bookmarks service usage

--- src/app/tv/search/search.component.ts
+++ src/app/tv/search/search.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {TvmazeService} from '../tvmaze.service';
 import {Show} from '../tv.models';
+import {BookmarksService} from '../../bookmarks/bookmarks.service';
 
 @Component({
   selector: 'tm-search',
@@ -11,14 +12,27 @@ export class SearchComponent implements OnInit {
   shows: Show[] = [];
   query = 'flash';
 
-  constructor(private tv: TvmazeService) {
+  constructor(private tv: TvmazeService,
+              private bs: BookmarksService) {
   }
 
   search(query: string): void {
     this.tv.searchShows(query)
       .subscribe(shows => this.shows = shows);
   }
+
   ngOnInit() {
   }
 
+  getAllBookmarks(): Show[] {
+    return this.bs.getAll() as Show[];
+  }
+
+  saveBookmark(show: Show): void {
+    this.bs.add(show);
+  }
+
+  isBookmark(show: Show): boolean {
+    return this.bs.has(show.id);
+  }
 }
