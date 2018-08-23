aeed40fdc3173e3726c9ae337208b8d5e8a76bd4
pawel.koselski <pawel.koselski@aptitudesoftware.com>
2018-08-23 09:43:52 +0200

    Ex. 64b - Episodize pipe refactoring #3

--- src/app/tv/episodize.pipe.ts
+++ src/app/tv/episodize.pipe.ts
@@ -1,14 +1,15 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { Episode } from './tv.models';
 
+const pad = val => val > 9 ? val.toString() : '0' + val;
+
 @Pipe({
   name: 'episodize'
 })
 export class EpisodizePipe implements PipeTransform {
-  private pad = val => val > 9 ? val.toString() : '0' + val;
 
   transform({number, season}: Episode): string {
-    const [numberPadded, seasonPadded] = [number, season].map(this.pad);
+    const [numberPadded, seasonPadded] = [number, season].map(pad);
 
     return `s${seasonPadded}e${numberPadded}`;
   }
